31279f327ead3724e9242c2cfc4fb85763d28c06
Ben Mor <benmor@nomail.com>
2021-12-27 21:36:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ECommerce backend app
+
+## with Java 11 and Spring
+
+based on [this tutorial](https://dev.to/nilmadhabmondal/lets-develop-an-e-commerce-application-from-scratch-using-java-and-spring-28go)
---
+++ src/main/java/com/benmorant/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.benmorant.ecommerce.repository;
+
+
+import com.benmorant.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategoryName(String categoryName);
+
+}
